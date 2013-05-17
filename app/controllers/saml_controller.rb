74c9fffbf25cc37630e59ec567c16bc5c74c9489
lapygithub <mlapinskas@appenbutlerhill.com>
2013-05-16 17:03:25 -0700

    saml_controller changes due to OneLogin ruby-saml differences
    Mostly settings were moved from .new to create/generate

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -12,7 +12,8 @@ class SamlController < ApplicationController
       return 
     end
 
-    request = Onelogin::Saml::Authrequest.new(@settings)
+=begin #calh - Now pass settings on create
+    request = Onelogin::Saml::Authrequest.new(@settings)  
 
     # Create the request, returning an action type and associated content
     action, content = request.create
@@ -24,6 +25,11 @@ class SamlController < ApplicationController
       # for POST requests (form) render the content as HTML
       render :inline => content
     end
+=end #calh - Now pass settings on create
+    request = Onelogin::Saml::Authrequest.new
+    content = request.create(@settings)
+    logger.debug "Saml Index request content: #{content}"
+    redirect_to content
   end
 
   def consume
@@ -33,8 +39,11 @@ class SamlController < ApplicationController
 
     respond_to do |format|
       if @response.is_valid?
-        session[:userid] = @response.name_id
+        session[:userid] = @settings.name_identifier_value = @response.name_id
+        
         session[:attributes] = @response.attributes
+        logger.debug "Session attributes after response: #{session[:attributes]}"
+
         format.html {
           if session[:goback_to] != nil
             redirect_to session[:goback_to]
@@ -75,15 +84,18 @@ class SamlController < ApplicationController
   def sp_logout_request
 
     # LogoutRequest accepts plain browser requests w/o paramters 
-    logout_request = Onelogin::Saml::LogoutRequest.new( :settings => @settings )
+#calh    logout_request = Onelogin::Saml::LogoutRequest.new( :settings => @settings )
+    logout_request = Onelogin::Saml::Logoutrequest.new()
 
     # Since we created a new SAML request, save the transaction_id 
     # to compare it with the response we get back
-    session[:transaction_id] = logout_request.transaction_id
+#calh    session[:transaction_id] = logout_request.transaction_id
+    session[:transaction_id] = logout_request.uuid
 
     logger.info "New SP SLO for userid '#{session[:userid]}'"
 
     # Create a new LogoutRequest for this session Name ID
+=begin #calh
     action, content = logout_request.create( :name_id => session[:userid] )
     case action
     when "GET"
@@ -93,12 +105,18 @@ class SamlController < ApplicationController
       # for POST requests (form) render the content as HTML
       render :inline => content
     end
+=end #calh
+    # Consume may not be called to set name_id_value if ruby sessions starts and stops
+    @settings.name_identifier_value = session[:userid] if @settings.name_identifier_value.blank?
+    logger.info "Session userid = #{session[:userid]}, settings.name_identifier_value=#{@settings.name_identifier_value}"
+    content = logout_request.create( @settings )
+    redirect_to content
   end
 
   # After sending an SP initiated LogoutRequest to the IdP, we need to accept
   # the LogoutResponse, verify it, then actually delete our session.
   def logout_response
-    logout_response = Onelogin::Saml::LogoutResponse.new( :response => params[:SAMLResponse], :settings => @settings )
+    logout_response = Onelogin::Saml::Logoutresponse.new( :response => params[:SAMLResponse], :settings => @settings )
 
     logger.info "LogoutResponse is: #{logout_response.to_s}"
 
@@ -127,7 +145,7 @@ class SamlController < ApplicationController
 
   # Method to handle IdP initiated logouts
   def idp_logout_request
-    logout_request = Onelogin::Saml::LogoutRequest.new( :request => params[:SAMLRequest], :settings => @settings)
+    logout_request = Onelogin::Saml::Logoutrequest.new( :request => params[:SAMLRequest], :settings => @settings)
     unless logout_request.is_valid?
       logger.error "IdP initiated LogoutRequest was not valid!"
     end
@@ -142,7 +160,7 @@ class SamlController < ApplicationController
 
     # Generate a response to the IdP.  :transaction_id sets the InResponseTo
     # SAML message to create a reply to the IdP in the LogoutResponse.
-    action, content = logout_response = Onelogin::Saml::LogoutResponse.new(:settings => @settings).
+    action, content = logout_response = Onelogin::Saml::Logoutresponse.new(:settings => @settings).
       create(:transaction_id => logout_request.transaction_id)
 
     case action
@@ -164,8 +182,11 @@ class SamlController < ApplicationController
       return
     end
 
+=begin #calh - Settings for metadata now set on generate
     meta = Onelogin::Saml::Metadata.new(@settings)
-    render :xml => meta.generate
+=end #calh
+    meta = Onelogin::Saml::Metadata.new
+    render :xml => meta.generate(@settings)
   end
 
   private 
@@ -181,5 +202,6 @@ class SamlController < ApplicationController
   def delete_session
     session[:userid] = nil
     session[:attributes] = nil
+    @settings = nil
   end
 end
